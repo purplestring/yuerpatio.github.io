932caa08c155f97ca217b983aae4f3e2f98a5a54
purplestring <purplestring@sina.cn>
2018-12-19 12:44:24 +0800

    bugs

--- group/group.js
+++ group/group.js
@@ -21,3 +21,18 @@ var shutTip = oShutTip || "";
 		}
 	}
 }
+
+function toZero(num){
+	if(num<=9){
+		return '000' + num;
+	}
+	else if(num<=99){
+		return '00' + num;
+	}
+	else if(num<=999){
+		return '0' + num;
+	}
+	else{
+		return '' + num;
+	}
+}
