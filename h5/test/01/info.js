5081a844e4c850444531f5bda7b3e1e0265d6f4b
purplestring <purplestring@sina.cn>
2019-08-05 12:36:53 +0800

    test

--- h5/test/01/info.js
+++ h5/test/01/info.js
@@ -3,9 +3,9 @@ var slidepics=[
 { "img":"images/0.jpg","info":"夫子庙的夜晚夫子庙的夜晚夫子庙的"},
 { "img":"images/a.jpg","info":"秦淮河的故事秦淮河的故事"},
 { "img":"images/b.jpg","info":"集贤亭的风光集贤亭的风光集贤亭的"},
-{ "img":"images/c.jpg","info":"茅家铺的清新茅家铺的清新茅家铺的清新"},
+/*{ "img":"images/c.jpg","info":"茅家铺的清新茅家铺的清新茅家铺的清新"},
 { "img":"images/d.jpg","info":"美丽洲的神秘"},
-{ "img":"images/e.jpg","info":"曲院风荷的优雅曲院风荷的优雅曲"},
+{ "img":"images/e.jpg","info":"曲院风荷的优雅曲院风荷的优雅曲"},*/
 ];
 	
 //Waterfall
@@ -24,7 +24,33 @@ var waterpics=[
 { "img":"images/12.jpg","info":"曲院风荷的优雅曲院风荷的优雅曲院风荷的优"},
 ];	
 
+//News
+var news=[
+{ "date":"Wednesday, 10 August 2011 4:14","title":"子庙的夜晚夫子庙的夜晚","info":"At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga."},
+{ "date":"Tuesday, 2 August 2011 16:32","title":"曲院风荷的优雅曲院风荷的","info":"At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga."},
+{ "date":"Monday, 8 August 2011 6:18","title":"茅家铺的清新茅家铺的清新","info":"At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga."},
+{ "date":"Monday, 8 August 2013 9:18","title":"茅家铺的清新茅的清新","info":"At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga."},
+];	
+
+var lang=["EN","中"];
+
 var navinames= [
 { "Home":"Home","About":"About","Photos":"Photos","News":"News","Contacts":"Contacts"},
 { "Home":"首页","About":"关于","Photos":"照片","News":"最新","Contacts":"联系"},
 ];
+
+var address= [
+["Our Contacts","Country:","USA","State:","California","City:","San Diego","Telephone:","+354 5635600","Email:","smartnet@mail.com"],
+["联系方式","省份:","淮海省","城市:","桐滨市","地址:","乌古木大街117号","电话:","1585635600X","Email:","abc@abc.com"],
+];
+
+var contactform= [
+["Contact Form","Your Name:","Your E-mail:","Your Message:","Send","Clear"],
+["留言板","姓名:","邮箱:","内容:","发送","清空"],
+];
+
+var misinfo= [
+["Miscellaneous Info:","Namlibero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudi andae sint et molestiae non recusandae itaque earum rerum hic. Namlibero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo."],
+["更多信息：","集贤亭的风光集贤亭的风光集贤亭的风光集贤亭的风光集贤亭的风光集贤亭的风光集贤亭的风光集贤亭的风光集贤亭的风光"],
+];
+
