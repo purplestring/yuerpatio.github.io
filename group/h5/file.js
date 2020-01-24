d4b335f810682f012595c7b984d976a04732fdbd
purplestring <purplestring@sina.cn>
2020-01-24 20:08:07 +0800

    h5

--- group/h5/file.js
+++ group/h5/file.js
@@ -34,6 +34,7 @@
 {"TITLE":"微博集卡抽奖","F_GROUP":"svg","S_GROUP":"动图","F_NAME":"weibojkcj.html","SHOW":"no",},
 {"TITLE":"火箭起飞01","F_GROUP":"svg","S_GROUP":"动图","F_NAME":"rocket01.html",},
 {"TITLE":"疫情地图","F_GROUP":"svg","S_GROUP":"动图","F_NAME":"map-2019-nCoV.html",},
+{"TITLE":"疫情地图Demo","F_GROUP":"svg","S_GROUP":"动图","F_NAME":"map-2019-nCoV-Demo.html","SHOW":"no",},
 {"TITLE":"四季01","F_GROUP":"svg","S_GROUP":"动图","F_NAME":"season01.html","SHOW":"no",},
 {"TITLE":"下雪01","F_GROUP":"svg","S_GROUP":"动图","F_NAME":"snow01.html","SHOW":"no",},
 {"TITLE":"寒江雪1","F_GROUP":"svg","S_GROUP":"动图","F_NAME":"hjx01.html","SHOW":"no",},
