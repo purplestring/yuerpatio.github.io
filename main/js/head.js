2b19612c78565fe9fa298b470323c61f48c65491
axvq <axvq@sina.cn>
2020-12-03 18:56:07 +0800

    20201203

--- main/js/head.js
+++ main/js/head.js
@@ -4,11 +4,11 @@
 "menu":["Menu","菜单"],
 };
 
-var page= ["book","paint","drug","bird","photo","pano","mypaint","h5"];
+var page= ["book","paint","photo","drug","bird","pano","mypaint","h5"];
 
 var navinames= [
-{ "book":"Books","paint":"Paints","drug":"Drugs","bird":"Birds","photo":"Photos","pano":"Panoramas","mypaint":"My Paints","h5":"My H5s"},
-{ "book":"书卷集","paint":"画卷集","drug":"本草集","bird":"百鸟集","photo":"百物卷","pano":"全景卷","mypaint":"书画页","h5":"设计页"},
+{ "book":"Books","paint":"Paints","photo":"Photos","drug":"Drugs","bird":"Birds","pano":"Panoramas","mypaint":"My Paints","h5":"My H5s"},
+{ "book":"书卷集","paint":"画卷集","photo":"万叶集","drug":"本草卷","bird":"百鸟卷","pano":"全景卷","mypaint":"书画页","h5":"设计页"},
 ];
 
 //var current=page.findIndex(function(d){return d == cpageName;});
@@ -17,3 +17,10 @@ var langid=1;
 if(sessionStorage.getItem("langid")==null) sessionStorage.setItem("langid",langid);
 else langid=sessionStorage.getItem("langid");
 var state=1;
+
+function linkTo(link){
+	let group=link.split('/')[0];
+	let name=link.split('/')[1];
+	if(group.split('_')[1]!=null) return '../'+group.replace(/_/,"/")+'.html?wd='+name;
+	else return '../'+group+'/index.html?wd='+name;
+}
