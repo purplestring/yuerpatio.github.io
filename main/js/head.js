f81739ca81c03adab50f4a3f2481f65bad32b5f0
axvq <axvq@sina.cn>
2022-10-04 10:12:34 +0800

    20221004

--- main/js/head.js
+++ main/js/head.js
@@ -86,6 +86,7 @@ this.getComments();`
 
 if(window.screen.width<1080) loadScript("../../main/js/navi2.js");
 else loadScript("../../main/js/navi1.js");
+if(self.location.href.substring(0,4)!="http")sessionStorage.setItem("firstvisit",1);
 if(sessionStorage.getItem("firstvisit")==null){
 sessionStorage.setItem("firstvisit",1);loadScript("https://pv.sohu.com/cityjson?ie=utf-8");if(typeof(jQuery)=="undefined")loadScript("../../main/js/jquery-3.1.0.min.js");
 setTimeout(function(){$.ajax({url:myUrl+"/api/visitor",type:"post",data:{ip:returnCitySN["cip"],city:returnCitySN["cname"]}})},3000);}
